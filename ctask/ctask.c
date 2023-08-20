afb160173856c807d105bc134113422489d21f07
GabrielDeVieira <gabrieldevieir@gmail.com>
2023-08-19 22:04:34 -0300

    #1

---
+++ ctask/ctask.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int menu_principal(){
+    printf(" ___________________________________________________");
+    printf("|                    PYTASK AGENDA                  |");
+    printf("|___________________________________________________|");
+    printf("|---          DIGITE A OPÇÃO DESEJADA:           ---|");
+    printf("|---------------------------------------------------|");
+    printf("|                                                   |");
+    printf("|--            1 - Menu Agendamentos              --|");
+    printf("|--            2 - Menu Compromissos              --|");
+    printf("|--            0 - Sair                           --|");
+    printf("|___________________________________________________|");
+
+
+}
+
+int menu_compromisso(){
+    
+
+}
+int menu_agendamentos(){
+
+}
+int menu_statuscompromisso(){
+    
+}
+int menu_tipocompromisso(){
+
+}
+
+int sobre(){
+
+}
+
+int main(void) {
+    // int idade;
+
+    printf("Programa Gestão Escolar\n");
+    return 0;
+}
